1f10a2f11b35f3fbf23c396bfeef51ea36a24833
KnOkEr350 <rootimt590@gmail.com>
2026-03-07 00:05:44 +0300

    " "

--- app/models.py
+++ app/models.py
@@ -12,7 +12,6 @@ class Item(Base):
 
 class Weather(Base):
     __tablename__ = "weather"
-
     id = Column(Integer, primary_key=True, index=True)
     city = Column(String, nullable=False, unique=True)
     temperature = Column(Float, nullable=False)
